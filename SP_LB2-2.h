f57b79fff23ad125197fdc8953e3f301315eadb6
Alex <99472724+AlexPol1ak@users.noreply.github.com>
2024-09-07 11:59:53 +0300

    Добавьте файлы проекта.

---
+++ SP_LB2-2.h
@@ -0,0 +1,9 @@
+#include "framework.h"
+#include "resource.h"
+#include "Windows.h"
+
+
+/// <summary>
+/// About
+/// </summary>
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
